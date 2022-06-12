571f1b533b8f01113a2afdcd97eb3e56634e6b78
raiko <raikobob@yandex.ru>
2022-06-12 23:17:06 +0300

    Реализовать собственную структуру данных - HashMap

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -32,16 +32,3 @@ public class User {
         return Objects.hash(name, children, birthday);
     }
 }
-
-class Main {
-    public static void main(String[] args) {
-        User user1 = new User("Ivan", 1,
-                new GregorianCalendar(1988, 10, 10));
-        User user2 = new User("Ivan", 1,
-                new GregorianCalendar(1988, 10, 10));
-        Map<User, Object> map = new HashMap<>();
-        map.put(user1, new Object());
-        map.put(user2, new Object());
-        System.out.println(map);
-    }
-}
