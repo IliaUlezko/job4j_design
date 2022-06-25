b52c2e5377be3bada749703b1f4889a0dae81935
raiko <raikobob@yandex.ru>
2022-06-25 22:15:03 +0300

    BufferedOutputStream

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,16 +1,17 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
     public List<String> filter(String file) {
         List<String> list = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader in = new BufferedReader(
+                new FileReader(file)
+        )) {
             list = in.lines()
                     .filter(line -> line.contains(" 404 "))
                     .collect(Collectors.toList());
@@ -20,9 +21,33 @@ public class LogFilter {
         return list;
     }
 
+    /**
+     * Метод работает не корректно,
+     * перезаписывает в файл последнюю
+     * итеррируемую строку.
+     * Подскажите, как исправить.
+     */
+    public static void save(List<String> log, String file) {
+        Iterator<String> iterator = log.iterator();
+        while (iterator.hasNext()) {
+            try (PrintWriter out = new PrintWriter(
+                    new BufferedOutputStream(
+                            new FileOutputStream(file)
+                    ))) {
+                out.printf("%s%n", iterator.next());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
         log.forEach(System.out::println);
+        System.out.println("--------------");
+        LogFilter logFilter1 = new LogFilter();
+        List<String> log1 = logFilter1.filter("log.txt");
+        save(log1, "404.txt");
     }
 }
