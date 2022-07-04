db1dc453025a1ddc72b1f724d86ef40e5068ed91
raiko <raikobob@yandex.ru>
2022-07-04 13:22:02 +0300

    Анализ доступности сервера

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(
+                new FileReader(source));
+        PrintWriter out = new PrintWriter(
+                new FileOutputStream(target))) {
+            boolean b = true;
+            while (in.ready()) {
+                String[] str = in.readLine().split(" ");
+                if (("400".contains(str[0]) || "500".contains(str[0])) && b) {
+                    out.print(str[1] + ";");
+                    b = false;
+                }
+                if (("200".contains(str[0]) || "300".contains(str[0])) && !b) {
+                    out.println(str[1] + ";");
+                    b = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("./data/server.log",
+                "./data/unavailableServer.csv");
+    }
+}
