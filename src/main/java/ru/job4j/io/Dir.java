8233239d37d9b3c7151e0f5dcf652347e292545c
raiko <raikobob@yandex.ru>
2022-07-06 11:50:38 +0300

    File

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("C:\\Projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format(
+                    "Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format(
+                    "Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.printf(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getAbsoluteFile());
+        }
+        for (File sub : file.listFiles()) {
+            System.out.println(String.format("File name is %s. File length is %s bytes.",
+                    sub.getName(), sub.length()));
+        }
+    }
+}
