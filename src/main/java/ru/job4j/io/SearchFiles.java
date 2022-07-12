dcbc80e7a3c0a20233f84046e3d4292c0f7cb0b0
raiko <raikobob@yandex.ru>
2022-07-12 20:26:24 +0300

    Сканирование файловой системы.

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -32,7 +32,7 @@ public class SearchFiles implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (condition.test(file)) {
-            files.add(file.toAbsolutePath());
+            files.add(file);
         }
         return CONTINUE;
     }
